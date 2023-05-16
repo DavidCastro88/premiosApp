e8d836c7079b1a98eafaf8d9ef97cb2c48eaaabb
DavidCastro88 <dcastrot@unal.edu.co>
2023-05-15 19:45:04 -0500

    Test, Cambios al admin, views, model. Base del front

--- premiosapp/polls/tests.py
+++ premiosapp/polls/tests.py
@@ -1,3 +1,121 @@
-from django.test import TestCase
+import datetime 
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+from .models import Question
+from django.urls.base import reverse
+
+#Testeamos, modelos o vistas
+class QuestionModelTests(TestCase):
+
+    def test_was_published_recently_with_future_questions(self):
+        """was published recently return False for questions pub_date in the future"""
+        time = timezone.now()+datetime.timedelta(days=30)
+        future_question= Question(question_text='¿Cuál es el mejor curso de IA?',pub_date=time)
+        self.assertIs(future_question.was_published_recently(),False)
+    
+    def test_was_published_recently_with_present_question(self):
+        """was published recently return True for questions pub_date in the present"""
+        time = timezone.now()
+        present_question = Question(question_text='¿Cual es el mejor curso de IA?',pub_date=time)
+        self.assertIs(present_question.was_published_recently(),True)
+    
+    def test_was_published_recently_with_less_than_a_day_gone(self):
+        """was published recently return True for question pub_date with less than a day gone"""
+        time=timezone.now() - datetime.timedelta(days=1)
+        past_question_day_gone=Question(question_text='¿Cual es el mejor curso de IA?',pub_date=time)
+        self.assertIs(past_question_day_gone.was_published_recently(),True)
+
+    def test_was_published_with_more_than_one_day_past(self):
+        """was published recently return False for questions pub_date with more than a day gone"""
+        time=timezone.now() - datetime.timedelta(days=2)
+        past_question_more_than_day= Question(question_text='¿Cual es el mejor curso de Ia?',pub_date=time)
+        self.assertIs(past_question_more_than_day.was_published_recently(),False)
+
+def create_question(question_text, days): 
+    """
+    Create a question with the given 'question_text' and published the given number
+    of 'days' offset to now (negative for questions published in the past, positive
+    for questions that have yet to be published).
+    """
+    time = timezone.now() + datetime.timedelta(days=days)
+    return Question.objects.create(question_text=question_text, pub_date=time)
+
+
+class QuestionIndexViewTests(TestCase):
+    
+    def test_no_questions(self):
+        """if no questions exist, an appropiate message is displayed """
+        response = self.client.get(reverse("polls:index"))
+        self.assertEqual(response.status_code,200)
+        self.assertContains(response, 'Not polls are Available')
+        self.assertQuerysetEqual(response.context['latest_question_list'], [])
+    
+    def test_no_future_question_are_displayed(self):
+        """If question future are create in DB,do not show up in the home"""
+        response = self.client.get(reverse("polls:index"))
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question=create_question(question_text='¿Question?',days=20)
+        self.assertContains(response, 'Not polls are Available')
+        self.assertNotIn(future_question, response.context["latest_question_list"])
+
+    def test_past_question(self):
+        """Questions with a pub_date in the past are displayed on the index page"""
+        question_past=create_question("Pregunta de prueba", days=-10)
+        response = self.client.get(reverse("polls:index"))
+        self.assertIn(question_past, response.context['latest_question_list'])
+
+    def test_future_question_and_past_question(self): 
+        """
+        Even if both past and future question exist, only past questions
+        are displayed.
+        """
+        question = create_question(question_text="Past question.", days=-30)
+        create_question(question_text="Future question.", days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [question],
+        )
+
+    def test_two_past_questions(self): 
+        """The questions index page may display multiple questions."""
+        question1 = create_question(question_text="Past question 1", days=-30)
+        question2 = create_question(question_text="Past question 2", days=-5)
+        response = self.client.get(reverse('polls:index'))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            [question2, question1],
+        )
+
+    def test_two_future_question(self):
+        """The questions index page may display multiple questions. """
+        question_future1 = create_question(question_text="future1_question",days=30)
+        question_future2 = create_question(question_text="future2_QUESTION",days=10)
+        response = self.client.get(reverse("polls:index"))
+        self.assertQuerysetEqual(
+            response.context['latest_question_list'],
+            []
+        )
+class QuestionDetailViewTests(TestCase):
+
+    def test_future_question(self):
+        """
+        The detail view of a question with a pub_date in the future
+        returns a 404 not found.
+        """
+        future_question=create_question(question_text="Future question.", days=30)
+        url = reverse('polls:detail',args=(future_question.id,))
+        response=self.client.get(url)
+        self.assertEqual(response.status_code, 404)
+        
+
+    def test_past_question(self):
+        """
+        The detail view of a question with a pub_date in the past
+        displays the question's text.
+        """
+        past_question=create_question(question_text="Past question.", days=-30)
+        url = reverse('polls:detail',args=(past_question.id,))
+        response=self.client.get(url)
+        self.assertContains(response, past_question.question_text)
--- premiosapp/polls/views.py
+++ premiosapp/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse,HttpResponseRedirect
 from .models import Question,Choice
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 """def index(request):
     latest_question_list=Question.objects.all()
@@ -26,13 +27,20 @@ class IndexView(generic.ListView):
     template_name = 'polls/index.html'
     context_object_name = 'latest_question_list'
 
-    def get_queryset(self):
-        #Return the last five published questions
-        return Question.objects.order_by('-pub_date')[:5]
+    def get_queryset(self): 
+        """Return the last five published questions (not including those set to be published in the future)."""
+        # return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     template_name='polls/detail.html'
     model = Question
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultView(generic.DetailView):
     template_name='polls/results.html'
